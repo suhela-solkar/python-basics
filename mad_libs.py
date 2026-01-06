306cc2b2206a1aa2a2bf488470fcc20ae8731314
Suhela Solkar <24ai15@aiktc.ac.in>
2026-01-06 19:26:19 +0530

    Create mad_libs.py

---
+++ mad_libs.py
@@ -0,0 +1,7 @@
+print("Welcome to Mad Libs!")
+name = input("Enter a name: ")
+place = input("Enter a place: ")
+food = input("Enter a food: ")
+
+story = f"{name} went to {place} to eat some {food}. It was delicious!"
+print("\nYour story:\n", story)
